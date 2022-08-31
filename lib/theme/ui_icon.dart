80324fc7914ae7ae48adc9196494f9962b4c323e
Charles Santos <csbs.conta@outlook.com>
2022-08-31 13:56:05 -0300

    [BottomNavigationWidget] criar

--- lib/theme/ui_icon.dart
+++ lib/theme/ui_icon.dart
@@ -1,6 +1,10 @@
 class UiIcon {
   static const String create = 'assets/icons/create.svg';
   static const String logo = 'assets/icons/logo.svg';
+  static const String logoActived = 'assets/icons/logo_actived.svg';
   static const String notification = 'assets/icons/notification.svg';
+  static const String notificationActived =
+      'assets/icons/notification_actived.svg';
   static const String options = 'assets/icons/options.svg';
+  static const String optionsActived = 'assets/icons/options_actived.svg';
 }
--- lib/widget/bottom_navigation_widget.dart
+++ lib/widget/bottom_navigation_widget.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:universe_history_app/theme/ui_color.dart';
 import 'package:universe_history_app/theme/ui_icon.dart';
 import 'package:universe_history_app/theme/ui_size.dart';
-import 'package:universe_history_app/theme/ui_theme.dart';
+
+ValueNotifier<String> currentPage = ValueNotifier('home');
 
 class BottomNavigationWidget extends StatefulWidget {
   const BottomNavigationWidget({Key? key}) : super(key: key);
@@ -13,41 +13,53 @@ class BottomNavigationWidget extends StatefulWidget {
 }
 
 class _BottomNavigationWidgetState extends State<BottomNavigationWidget> {
+  void changeIcon(String page) {
+    currentPage.value = page;
+  }
+
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder(
-      valueListenable: currentTheme,
-      builder: (BuildContext context, Brightness theme, _) {
-        return Container(
+      valueListenable: currentPage,
+      builder: (BuildContext context, String currentPage, _) {
+        return SizedBox(
           height: UiSize.bottomNavigation,
           width: double.maxFinite,
-          decoration: const BoxDecoration(
-            border: Border(
-              left: BorderSide(
-                color: UiColor.border,
-                width: UiSize.border,
-              ),
-            ),
-          ),
           child: Row(
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               IconButton(
-                icon: SvgPicture.asset(UiIcon.logo),
-                onPressed: () {},
+                icon: SvgPicture.asset(
+                  currentPage == 'home' ? UiIcon.logoActived : UiIcon.logo,
+                ),
+                onPressed: () {
+                  changeIcon('home');
+                },
               ),
               IconButton(
                 icon: SvgPicture.asset(UiIcon.create),
                 onPressed: () {},
               ),
               IconButton(
-                icon: SvgPicture.asset(UiIcon.notification),
-                onPressed: () {},
+                icon: SvgPicture.asset(
+                  currentPage == 'notification'
+                      ? UiIcon.notificationActived
+                      : UiIcon.notification,
+                ),
+                onPressed: () {
+                  changeIcon('notification');
+                },
               ),
               IconButton(
-                icon: SvgPicture.asset(UiIcon.options),
-                onPressed: () {},
+                icon: SvgPicture.asset(
+                  currentPage == 'settings'
+                      ? UiIcon.optionsActived
+                      : UiIcon.options,
+                ),
+                onPressed: () {
+                  changeIcon('settings');
+                },
               ),
             ],
           ),
