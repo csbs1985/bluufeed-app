7879064aefdf9cc309790b520ba32644c34ce08a
Charles Santos <csbs.conta@outlook.com>
2022-09-04 12:49:16 -0300

    [icones] otimizar

--- lib/theme/ui_icon.dart
+++ lib/theme/ui_icon.dart
@@ -1,13 +1,19 @@
 class UiIcon {
-  static const String create = 'assets/icons/create.svg';
-  static const String createActived = 'assets/icons/create_actived.svg';
+  static const String authorized = 'assets/icons/authorized.svg';
   static const String closed = 'assets/icons/closed.svg';
+  static const String create = 'assets/icons/create.svg';
+  static const String comment = 'assets/icons/comment.svg';
+  static const String createActived = 'assets/icons/create_actived.svg';
+  static const String favorite = 'assets/icons/favorite.svg';
+  static const String favoriteActived = 'assets/icons/favorited.svg';
+  static const String identity = 'assets/icons/identity.svg';
   static const String logo = 'assets/icons/logo.svg';
   static const String logoActived = 'assets/icons/logo_actived.svg';
-  static const String identity = 'assets/icons/identity.svg';
   static const String notification = 'assets/icons/notification.svg';
   static const String notificationActived =
       'assets/icons/notification_actived.svg';
-  static const String options = 'assets/icons/options.svg';
-  static const String optionsActived = 'assets/icons/options_actived.svg';
+  static const String more = 'assets/icons/more.svg';
+  static const String moreActived = 'assets/icons/more_actived.svg';
+  static const String option = 'assets/icons/option.svg';
+  static const String read = 'assets/icons/read.svg';
 }
