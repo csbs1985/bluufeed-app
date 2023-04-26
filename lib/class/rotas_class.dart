c8ee980899b76cbb74c0a161130546387d41b384
Charles Santos <csbs.conta@outlook.com>
2023-04-26 13:45:40 -0300

    [GoRouter] adicionar

---
+++ lib/class/rotas_class.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class RotasClass {
+  CustomTransitionPage buildPageWithDefaultTransition<T>({
+    required BuildContext context,
+    required GoRouterState state,
+    required Widget child,
+  }) {
+    return CustomTransitionPage(
+      key: state.pageKey,
+      child: child,
+      transitionDuration: const Duration(milliseconds: 300),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+          SlideTransition(
+        position: Tween<Offset>(
+          begin: const Offset(1, 0),
+          end: const Offset(0, 0),
+        ).animate(animation),
+        child: child,
+      ),
+    );
+  }
+}
+
+enum RouteEnum {
+  ENTRAR('/entrar'),
+  INICIO('/inicio');
+
+  final String value;
+  const RouteEnum(this.value);
+}
---
+++ lib/config/routes_config.dart
@@ -0,0 +1,37 @@
+import 'package:bluufeed_app/class/rotas_class.dart';
+import 'package:bluufeed_app/page/entrar_page.dart';
+import 'package:go_router/go_router.dart';
+
+final RotasClass _rotasClass = RotasClass();
+
+final GoRouter router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      pageBuilder: (context, state) =>
+          _rotasClass.buildPageWithDefaultTransition(
+        context: context,
+        state: state,
+        child: const EntrarPage(),
+      ),
+    ),
+    GoRoute(
+      path: RouteEnum.ENTRAR.value,
+      pageBuilder: (context, state) =>
+          _rotasClass.buildPageWithDefaultTransition(
+        context: context,
+        state: state,
+        child: const EntrarPage(),
+      ),
+    ),
+    GoRoute(
+      path: RouteEnum.INICIO.value,
+      pageBuilder: (context, state) =>
+          _rotasClass.buildPageWithDefaultTransition(
+        context: context,
+        state: state,
+        child: const EntrarPage(),
+      ),
+    ),
+  ],
+);
